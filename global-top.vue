2fd53e7a0a805febdac066acaaa2817f4806a5ed
kleinlennart <31488915+kleinlennart@users.noreply.github.com>
2026-02-10 14:50:26 -0500

    fix: update logo image path to match naming conventions

--- global-top.vue
+++ global-top.vue
@@ -1,5 +1,5 @@
 <template>
   <div v-if="$nav.currentPage > 1" class="absolute top-4 right-4 w-8">
-    <img src="/images/emblem/UN%20Emblem_Colour.svg" alt="UN Emblem" />
+    <img src="/images/emblem/un-emblem-colour.svg" alt="UN Emblem" />
   </div>
 </template>
